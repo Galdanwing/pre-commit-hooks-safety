24cb28043549779f680c297b3b5cfe552aa6363c
AntoinevdHorst <antoine.van.der.horst@target-holding.nl>
2020-02-29 12:20:58 +0100

    Add support for exclude

--- pre_commit_hooks/safety_check.py
+++ pre_commit_hooks/safety_check.py
@@ -1,17 +1,82 @@
-from __future__ import print_function
+import argparse
+import logging
+import os
+import subprocess
 import sys
+
 from safety.cli import check
 
+logger = logging.getLogger(__name__)
 
-def main(argv):
+
+def do_the_safety_check(args):
+    if args.requirements_file:
+        with open(args.requirements_file) as fp:
+            reqs = fp.read()
+    else:
+        reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze']).decode()
+
+    if args.exclude:
+        reqs = reqs.splitlines()
+        exclude = []
+        for item in args.exclude:
+            exclude.append(item + "==")
+
+        exclude = tuple(exclude)
+        for item in reqs:
+            if item.startswith(exclude):
+                reqs.remove(item)
+
+        reqs = """
+        {}
+        """.format(
+            "\n".join(reqs[1:])
+        )
+
+    with open("temp_file.txt", "w+") as file:
+        file.write(reqs)
     try:
-        check.main(['--full-report'] + sum((['-r', f] for f in argv), []))
-        return 0
+        check.main(['--full-report', '--file', 'temp_file.txt'])
     except SystemExit as error:
         if error.code == 0:
             return 0
         return 1
 
+    finally:
+        os.remove("temp_file.txt")
 
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+
+def main(args=None):
+    parser = argparse.ArgumentParser(
+        usage="""
+                           Download json files containing a list of CVE's and run the safety tool to check if the current environment(or specified file) contains vulnerabilities.
+                           Examples of usage:
+                           1: python /path/to/this/file/safety_script.py --download --safetycheck
+                           2: python /path/to/this/file/safety_script.py --safetycheck --exclude bleach mistune web3
+
+                           """
+    )
+
+    parser.add_argument(
+        "--requirementsfile",
+        dest="requirements_file",
+        type=str,
+        default=None,
+        help="Set if you want to run safety tool on a file instead of on environment. None by default",
+    )
+    parser.add_argument(
+        "--exclude",
+        dest="exclude",
+        type=str,
+        nargs="+",
+        default=None,
+        help="Specify which requirements you want to exclude",
+    )
+    args = parser.parse_args(args)
+
+    do_the_safety_check(args)
+    sys.exit(2)
+
+
+if __name__ == "__main__":
+    exit(main(sys.argv[1:]))
--- setup.py
+++ setup.py
@@ -2,23 +2,21 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='pre-commit-hooks-safety',
-    description='A pre-commit hook to check your Python dependencies against safety-db',
-    url='https://github.com/Lucas-C/pre-commit-hooks-safety',
-    version='1.1.0',
+    name='pre-commit-hooks-safety-extra',
+    description='A pre-commit hook to check your Python dependencies against safety-db, while also allowing you to exclude requirements',
+    # url='https://github.com/Lucas-C/pre-commit-hooks-safety',
+    version='1.0.0',
 
-    author='Lucas Cimon',
-    author_email='lucas.cimon@gmail.com',
+    author='Antoine van der Horst',
+    author_email='antoinevdhorst@gmail.com',
 
     platforms='linux',
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
